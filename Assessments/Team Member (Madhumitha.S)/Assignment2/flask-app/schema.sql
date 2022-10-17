377840b4f952f7d2cccb71fde51d956846558c97
Madhumitha131001 <114676701+Madhumitha131001@users.noreply.github.com>
2022-10-17 15:16:22 +0530

    Add files via upload

---
+++ Assessments/Team Member (Madhumitha.S)/Assignment2/flask-app/schema.sql
@@ -0,0 +1,21 @@
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT NOT NULL,
+    password TEXT NOT NULL,
+    name TEXT NOT NULL,
+    email TEXT NOT NULL
+);
+
+INSERT INTO users (username, password, name, email) VALUES (
+    "atp",
+    "mito@123",
+    "mito",
+    "anime@gmx.de"
+);
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Priva
